4abbe5479654d904accda2e8d68e5172c2506e1e
Anupamkush9 <anupam.kush9@gmail.com>
2024-06-11 08:26:08 +0530

    changed database from sqlite3 to postgresql

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Document
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'updated_at', 'description']
 
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'uploaded_file']
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Document, DocumentAdmin)
--- product/models.py
+++ product/models.py
@@ -10,3 +10,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=100)
+    uploaded_file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
