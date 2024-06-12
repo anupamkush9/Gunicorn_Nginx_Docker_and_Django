9884cc6a680d306fc33dd60c34b4601a4e322d87
Anupamkush9 <anupam.kush9@gmail.com>
2024-06-13 00:46:02 +0530

    Serving static and media files using NGINX

--- config/settings.py
+++ config/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static_volume/'
-STATIC_ROOT = os.path.join(BASE_DIR,"static_volume")
-MEDIA_URL = 'media_volume/'
-MEDIA_ROOT = os.path.join(BASE_DIR,"media_volume")
+STATIC_URL = '/static/'
+STATIC_ROOT = "/static/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
