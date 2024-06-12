c2766d58e555c2631578260505f84955baa8064c
Anupamkush9 <anupam.kush9@gmail.com>
2024-06-13 02:09:55 +0530

    serving-static-and-media-files-using-nginx-and-docker-named-volume concept explaination

--- config/settings.py
+++ config/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/static/"
+STATIC_ROOT = "/vol/static/"
 MEDIA_URL = '/media/'
-MEDIA_ROOT = "/media/"
+MEDIA_ROOT = "/vol/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
